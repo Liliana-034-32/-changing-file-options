7c58ccfa3772e641eb6bdda36bd2b7504f5666fa
Liliana <56549932+Liliana-034-32@users.noreply.github.com>
2022-08-25 14:08:27 +0500

    Add files via upload

---
+++ ovoch/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ovoch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            button1.BackColor = System.Drawing.Color.Snow;
+            button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            button1.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            button1.Location = new System.Drawing.Point(32, 40);
+            button1.Margin = new System.Windows.Forms.Padding(0);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(139, 52);
+            button1.TabIndex = 5;
+            button1.Text = "Выбрать файл";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(634, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(725, 270);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(725, 356);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(201, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(725, 433);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.button2.Location = new System.Drawing.Point(923, 504);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 69);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить изменения";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Пример ввода";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.label2.Location = new System.Drawing.Point(722, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Дата создания файла";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.label3.Location = new System.Drawing.Point(722, 323);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Дата изменения файла";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.label4.Location = new System.Drawing.Point(722, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Дата открытия файла";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(857, 72);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(224, 23);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.button3.Location = new System.Drawing.Point(857, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Форматировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(854, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(284, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Формат сохранения введеных изменений";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Window;
+            this.label6.Font = new System.Drawing.Font("Microsoft PhagsPa", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Image = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.label6.Location = new System.Drawing.Point(29, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Выбранный файл:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.BackgroundImage = global::ovoch.Properties.Resources._1616988818_4_p_fon_dlya_prilozheniya_4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1157, 614);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ovoch/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ovoch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox5.Text = showTime(DateTime.Now.ToString());
+
+
+            this.label1.Text = "Пример ввода：" + DateTime.Now.Year.ToString() + "  -год-  " + DateTime.Now.Month.ToString() + "  -месяц-  " + DateTime.Now.Day.ToString() + "  -день-  " + DateTime.Now.Hour.ToString() + "  -час-  " + DateTime.Now.Minute.ToString() + "  -минута-  " + DateTime.Now.Second.ToString() + "  -секунда  \n　　　Пожалуйста, напишите так：  " + showTime(DateTime.Now.ToString());
+
+
+        }
+
+        public string showTime(string Time)
+
+
+        {
+
+
+            if (Time.IndexOf("time") != -1)
+
+
+                Time = (Time.Remove(Time.IndexOf("time") + 3, 2).Insert(Time.IndexOf("time") + 3, (int.Parse(Time.Substring(Time.IndexOf("time") + 3, 2)) + 12).ToString())).Replace("time", "");
+
+
+            if (Time.IndexOf("time") != -1)
+
+
+                Time = (Time.Remove(Time.IndexOf("time") + 3, 2).Insert(Time.IndexOf("time") + 3, (int.Parse(Time.Substring(Time.IndexOf("time") + 3, 2)) + 12).ToString())).Replace("time", "");
+
+
+            return Time;
+
+
+        }
+
+
+
+
+
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+
+
+            {
+
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+
+
+                {
+
+
+                    this.textBox1.Text = ofd.FileName;
+
+
+                    this.textBox2.Text = showTime(System.IO.File.GetCreationTime(this.textBox1.Text).ToString());
+
+
+                    this.textBox3.Text = showTime(System.IO.File.GetLastWriteTime(this.textBox1.Text).ToString());
+
+
+                    this.textBox4.Text = showTime(System.IO.File.GetLastAccessTime(this.textBox1.Text).ToString());
+
+
+                }
+
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            {
+
+
+                try
+
+
+                {
+
+
+                    System.IO.File.SetCreationTime(this.textBox1.Text, DateTime.Parse(textBox2.Text));
+
+
+                    System.IO.File.SetLastWriteTime(this.textBox1.Text, DateTime.Parse(textBox3.Text));
+
+
+                    System.IO.File.SetLastAccessTime(this.textBox1.Text, DateTime.Parse(textBox4.Text));
+
+
+                    MessageBox.Show("Успешно модифицировано");//Успешно модифицировано 修改成功
+
+
+                }
+
+
+                catch
+
+
+                {
+
+
+                    MessageBox.Show("Вы не выбрали файл!"); //Вы не выбрали файл!您沒有選取檔案！
+
+
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+
+            Clipboard.SetData(DataFormats.Text, this.textBox5.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
